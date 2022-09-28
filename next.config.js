d2861c5ed5ceb6267d89afd7b3e803652fbce6e7
Terry Tan <tanjunweiterry@gmail.com>
2022-09-28 12:34:49 +0800

    Basic setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      /* "lh3.googleusercontent.com", "firebasestorage.googleapis.com" */
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

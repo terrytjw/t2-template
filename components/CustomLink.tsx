449a7d95c4ce7e5e511582c7259fba645496b123
terrytjw <tanjunweiterry@gmail.com>
2023-01-07 00:19:51 +0800

    added CustomLink and Loading components

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
 import React from "react";
 
-const CustomLink = () => {
-  return <div>CustomLink</div>;
+type CustomLinkProps = {
+  href: string;
+  openInNewPage?: boolean;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+  children: React.ReactNode;
+};
+const CustomLink = ({
+  href,
+  openInNewPage,
+  className,
+  onClick,
+  children,
+}: CustomLinkProps) => {
+  return (
+    <Link
+      href={href}
+      className={`px-2 inline-flex items-center w-fit hover:text-gray-400 transition-all ${className}`}
+      target={openInNewPage ? `_blank` : `_self`}
+      rel="noreferrer"
+      onClick={onClick}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default CustomLink;
